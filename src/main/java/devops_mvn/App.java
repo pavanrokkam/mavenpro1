eba66135c4566bd24bbd146b0f2cb9f9a33fd534
pavan rokkam <pavanrokkam4@gmail.com>
2021-10-03 23:31:46 +0530

    maven project files

---
+++ src/main/java/devops_mvn/App.java
@@ -0,0 +1,13 @@
+package devops_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
